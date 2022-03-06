8d1f4ea20ec176d7f5ef467b21259b611b7f998e
GalcaVlad <vladgalca@gmail.com>
2022-03-06 17:11:21 +0200

    Finished basis of Menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,20 +1,18 @@
 import React from "react";
 import "../assets/styles/Menu.css";
-import menuIcon from "../assets/images/icon-menu.svg";
 
 function toggleMenu() {
-  document.getElementById("menu-options").classList.toggle("closed");
+  document.getElementById("options").classList.toggle("closed");
 }
 
 export default function Menu() {
   return (
-    <div id="menu-container">
-      <div id="menu-toggle" className="menu-button" onClick={toggleMenu}>
-        <img src={menuIcon} className="icon" />
-      </div>
-      <div id="menu-options" className="closed">
-        <div id="auto-rotate" className="menu-button" />
-      </div>
+    <div className="menu-container">
+      <div className="menu-toggle menu-button" onClick={toggleMenu}></div>
+      <ul id="options" className="menu-options closed">
+        <li className="auto-rotate menu-button" />
+        <li className="menu-button" />
+      </ul>
     </div>
   );
 }
